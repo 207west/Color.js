32e078fd7dcb99eef5894d2e88e05f06d9686b3c
Adam Drago <atdrago@gmail.com>
2012-08-07 17:41:39 -0400

    Switched local function definitions to anonymous

--- Color.js
+++ Color.js
@@ -81,98 +81,99 @@
         }
     };
 
-    function toString(o) {
-        return Object.prototype.toString.call(o);
-    }
-    function isArray(o) {
-        return toString(o) === '[object Array]';
-    }
-    function isString(o) {
-        return toString(o) === '[object String]';
-    }
-    function map(arr, fn) {
-        for (var i = 0; i < arr.length; i++) {
-            arr[i] = fn(arr[i]);
-        }
-        return arr;
-    }
-    function pad(str, length, padding) {
-        while (str.length < length) {
-            str = padding + str;
-        }
-        return str;
-    }
-    function hsvToRgba(hsv) {
-        var r, g, b,
-            h = hsv[0],
-            s = hsv[1],
-            v = hsv[2],
-            i = Math.floor(h * 6),
-            f = h * 6 - i,
-            p = v * (1 - s),
-            q = v * (1 - f * s),
-            t = v * (1 - (1 - f) * s);
-
-        switch (i % 6) {
-            case 0: r = v, g = t, b = p; break;
-            case 1: r = q, g = v, b = p; break;
-            case 2: r = p, g = v, b = t; break;
-            case 3: r = p, g = q, b = v; break;
-            case 4: r = t, g = p, b = v; break;
-            case 5: r = v, g = p, b = q; break;
-        }
-
-        return [Math.floor(r * 255), Math.floor(g * 255), Math.floor(b * 255), 1];
-    }
-    function rgbToHsv(rgb) {
-        var r = rgb[0]/255,
-            g = rgb[1]/255,
-            b = rgb[2]/255,
-            max = Math.max(r, g, b),
-            min = Math.min(r, g, b),
-            h = 0,
-            v = max,
-            d = max - min,
-            s = max == 0 ? 0 : d / max;
-
-        if(max == min){
-            h = 0;
-        }else{
-            switch(max){
-                case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-                case g: h = (b - r) / d + 2; break;
-                case b: h = (r - g) / d + 4; break;
+    // Local helper functions
+    var toString = function (o) {
+            return Object.prototype.toString.call(o);
+        },
+        isArray = function (o) {
+            return toString(o) === '[object Array]';
+        },
+        isString = function (o) {
+            return toString(o) === '[object String]';
+        },
+        map = function (arr, fn) {
+            for (var i = 0; i < arr.length; i++) {
+                arr[i] = fn(arr[i]);
             }
-            h /= 6;
-        }
+            return arr;
+        },
+        pad = function (str, length, padding) {
+            while (str.length < length) {
+                str = padding + str;
+            }
+            return str;
+        },
+        hsvToRgba = function (hsv) {
+            var r, g, b,
+                h = hsv[0],
+                s = hsv[1],
+                v = hsv[2],
+                i = Math.floor(h * 6),
+                f = h * 6 - i,
+                p = v * (1 - s),
+                q = v * (1 - f * s),
+                t = v * (1 - (1 - f) * s);
 
-        return [h, s, v];
-    }
-    function rgbToHex(rgb) {
-        return pad(rgb[0].toString(16), 2, "0") + 
-               pad(rgb[1].toString(16), 2, "0") +
-               pad(rgb[2].toString(16), 2, "0");
-    }
-    function hexToRgba(hex) {
-        hex = fixHex(hex);
-        return [parseInt(hex.substr(0, 2), 16), 
-                parseInt(hex.substr(2, 2), 16), 
-                parseInt(hex.substr(4, 2), 16)];
-    }
-    function rgbaToRgb(rgba) {
-        rgba.pop();
-        return rgba;
-    }
-    function rgbToRgba(rgb) {
-        if (rgb.length == 3) {
-            rgb.push(1.0);
-        }
-        return rgb;
-    }
-    function fixHex(hex) {
-        hex = (hex[0] == "#" ? hex.substring(1) : hex); // remove # if it's there
+            switch (i % 6) {
+                case 0: r = v, g = t, b = p; break;
+                case 1: r = q, g = v, b = p; break;
+                case 2: r = p, g = v, b = t; break;
+                case 3: r = p, g = q, b = v; break;
+                case 4: r = t, g = p, b = v; break;
+                case 5: r = v, g = p, b = q; break;
+            }
 
-        // if the length is 3 (e.g., ABA), return the 6 digit form (e.g., AABBAA)
-        return hex.length == 3 ? (hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2]) : hex;
-    }
+            return [Math.floor(r * 255), Math.floor(g * 255), Math.floor(b * 255), 1];
+        },
+        rgbToHsv = function (rgb) {
+            var r = rgb[0]/255,
+                g = rgb[1]/255,
+                b = rgb[2]/255,
+                max = Math.max(r, g, b),
+                min = Math.min(r, g, b),
+                h = 0,
+                v = max,
+                d = max - min,
+                s = max == 0 ? 0 : d / max;
+
+            if (max == min) {
+                h = 0;
+            } else {
+                switch (max) {
+                    case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+                    case g: h = (b - r) / d + 2; break;
+                    case b: h = (r - g) / d + 4; break;
+                }
+                h /= 6;
+            }
+
+            return [h, s, v];
+        },
+        rgbToHex = function (rgb) {
+            return pad(rgb[0].toString(16), 2, "0") + 
+                   pad(rgb[1].toString(16), 2, "0") +
+                   pad(rgb[2].toString(16), 2, "0");
+        },
+        hexToRgba = function (hex) {
+            hex = fixHex(hex);
+            return [parseInt(hex.substr(0, 2), 16), 
+                    parseInt(hex.substr(2, 2), 16), 
+                    parseInt(hex.substr(4, 2), 16)];
+        },
+        rgbaToRgb = function (rgba) {
+            rgba.pop();
+            return rgba;
+        },
+        rgbToRgba = function (rgb) {
+            if (rgb.length == 3) {
+                rgb.push(1.0);
+            }
+            return rgb;
+        },
+        fixHex = function (hex) {
+            hex = (hex[0] == "#" ? hex.substring(1) : hex); // remove # if it's there
+
+            // if the length is 3 (e.g., ABA), return the 6 digit form (e.g., AABBAA)
+            return hex.length == 3 ? (hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2]) : hex;
+        };
 }).call(this);

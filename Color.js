91020e5b7680ed12ba70a8464ff80ae617c1ac75
Adam Drago <atdrago@gmail.com>
2013-09-05 12:10:18 -0400

    added faster min, max, and floor methods, added bootstrap, made test file better

--- Color.js
+++ Color.js
@@ -21,11 +21,17 @@
             return hsva === null ? null : hsvaToRgba(hsva);
         },
         getHex: function (withHash) {
-            var hsv = this.getHsv();
-            return hsv === null ? null : hsvToHex(hsv);
+            var hsv = this.getHsv(),
+                hex = hsv === null ? null : hsvToHex(hsv); 
+
+            if (hex && withHash) {
+                hex = '#' + hex;
+            }
+
+            return hex;
         },
         getHsv: function () {
-            var hsva = this.getHsva();
+            var hsva = this.hsva;
             return hsva === null ? null : hsva.slice(0, 3);
         },
         getHsva: function () {
@@ -59,17 +65,21 @@
                     this.hsva = rgbaToHsva(obj);
                 }
             } else if (isString(obj)) {
-                // if obj is a string, we treat it as hex unless in the format rgb(#,#,#) or rgba(#,#,#,#)
-                if (obj[0] == "r") {
-                    // this is an rgb(a) string
-                    if (obj[3] != "a") {
-                        this.hsva = rgbaToHsva(map(rgbToRgba(obj.substring(4, obj.length - 1).split(",")), function (v) { return +v; }));
-                    } else {
-                        this.hsva = rgbaToHsva(map(obj.substring(5, obj.length - 1).split(","), function (v) { return +v; }));
-                    }
+                // if obj is a string, we check if it's a named color, if not, then treat it as hex unless in the format rgb(#,#,#) or rgba(#,#,#,#)
+                if (colorNames[obj]) {
+                    this.hsva = nameToHsva(obj);
                 } else {
-                    // this is a hex string
-                    this.hsva = hexToHsva(obj);
+                    if (obj[0] == "r") {
+                        // this is an rgb(a) string
+                        if (obj[3] != "a") {
+                            this.hsva = rgbaToHsva(map(rgbToRgba(obj.substring(4, obj.length - 1).split(",")), function (v) { return +v; }));
+                        } else {
+                            this.hsva = rgbaToHsva(map(obj.substring(5, obj.length - 1).split(","), function (v) { return +v; }));
+                        }
+                    } else {
+                        // this is a hex string
+                        this.hsva = hexToHsva(obj);
+                    }
                 }
             } else {
                 this.hsva = [0, 0, 0, 0]; // if nothing is specified, return transparent black
@@ -79,7 +89,36 @@
     };
 
     // Local helper functions
-    var toString = function (o) {
+    var MathFloor = function (n) {
+            return n | n;
+        },
+        MathMin = function () {
+            var min = arguments[0];
+
+            for (var i = arguments.length - 1; i > 0; i--) {
+                var arg = arguments[i];
+                
+                if (arg < min) {
+                    min = arg;
+                }
+            }
+
+            return min;
+        },
+        MathMax = function () {
+            var max = arguments[0];
+
+            for (var i = arguments.length - 1; i > 0; i--) {
+                var arg = arguments[i];
+                
+                if (arg > max) {
+                    max = arg;
+                }
+            }
+
+            return max;
+        },
+        toString = function (o) {
             return Object.prototype.toString.call(o);
         },
         isArray = function (o) {
@@ -108,7 +147,7 @@
                 h = hsv[0],
                 s = hsv[1],
                 v = hsv[2],
-                i = Math.floor(h * 6),
+                i = MathFloor(h * 6),
                 f = h * 6 - i,
                 p = v * (1 - s),
                 q = v * (1 - f * s),
@@ -123,14 +162,14 @@
                 case 5: r = v, g = p, b = q; break;
             }
 
-            return [Math.floor(r * 255), Math.floor(g * 255), Math.floor(b * 255)];
+            return [MathFloor(r * 255), MathFloor(g * 255), MathFloor(b * 255)];
         },
         rgbToHsv = function (rgb) {
             var r = rgb[0]/255,
                 g = rgb[1]/255,
                 b = rgb[2]/255,
-                max = Math.max(r, g, b),
-                min = Math.min(r, g, b),
+                max = MathMax(r, g, b),
+                min = MathMin(r, g, b),
                 h = 0,
                 v = max,
                 d = max - min,
@@ -164,7 +203,7 @@
             rgb.push(a);
 
             return rgb;
-        }
+        },
         rgbToHsva = function (rgb) {
             var hsv = rgbToHsv(rgb);
             hsv.push(1.0);
@@ -183,7 +222,14 @@
                     parseInt(hex.substr(4, 2), 16)];
         },
         hexToHsv = function (hex) {
-            return rgbToHsv(hexToRgba(fixHex(hex)));
+            var rgb;
+
+            hex = fixHex(hex);
+            rgb = [parseInt(hex.substr(0, 2), 16), 
+                   parseInt(hex.substr(2, 2), 16), 
+                   parseInt(hex.substr(4, 2), 16)];
+
+            return rgbToHsv(rgb);
         },
         hexToHsva = function (hex) {
             var hsv = hexToHsv(hex);
@@ -210,10 +256,179 @@
             return rgb;
         },
         fixHex = function (hex) {
+            var hex1, hex2, hex3;
+
             hex = (hex[0] == "#" ? hex.substring(1) : hex); // remove # if it's there
 
+            hex1 = hex[0];
+            hex2 = hex[1];
+            hex3 = hex[2];
+
             // if the length is 3 (e.g., ABA), return the 6 digit form (e.g., AABBAA)
-            return hex.length == 3 ? (hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2]) : hex;
+            return hex.length == 3 ? (hex1 + hex1 + hex2 + hex2 + hex3 + hex3) : hex;
+        },
+        colorNames = {
+            'aliceblue':'#f0f8ff',
+            'antiquewhite':'#faebd7',
+            'aqua':'#00ffff',
+            'aquamarine':'#7fffd4',
+            'azure':'#f0ffff',
+            'beige':'#f5f5dc',
+            'bisque':'#ffe4c4',
+            'black':'#000000',
+            'blanchedalmond':'#ffebcd',
+            'blue':'#0000ff',
+            'blueviolet':'#8a2be2',
+            'brown':'#a52a2a',
+            'burlywood':'#deb887',
+            'cadetblue':'#5f9ea0',
+            'chartreuse':'#7fff00',
+            'chocolate':'#d2691e',
+            'coral':'#ff7f50',
+            'cornflowerblue':'#6495ed',
+            'cornsilk':'#fff8dc',
+            'crimson':'#dc143c',
+            'cyan':'#00ffff',
+            'darkblue':'#00008b',
+            'darkcyan':'#008b8b',
+            'darkgoldenrod':'#b8860b',
+            'darkgray':'#a9a9a9',
+            'darkgreen':'#006400',
+            'darkkhaki':'#bdb76b',
+            'darkmagenta':'#8b008b',
+            'darkolivegreen':'#556b2f',
+            'darkorange':'#ff8c00',
+            'darkorchid':'#9932cc',
+            'darkred':'#8b0000',
+            'darksalmon':'#e9967a',
+            'darkseagreen':'#8fbc8f',
+            'darkslateblue':'#483d8b',
+            'darkslategray':'#2f4f4f',
+            'darkturquoise':'#00ced1',
+            'darkviolet':'#9400d3',
+            'deeppink':'#ff1493',
+            'deepskyblue':'#00bfff',
+            'dimgray':'#696969',
+            'dodgerblue':'#1e90ff',
+            'firebrick':'#b22222',
+            'floralwhite':'#fffaf0',
+            'forestgreen':'#228b22',
+            'fuchsia':'#ff00ff',
+            'gainsboro':'#dcdcdc',
+            'ghostwhite':'#f8f8ff',
+            'gold':'#ffd700',
+            'goldenrod':'#daa520',
+            'gray':'#808080',
+            'green':'#008000',
+            'greenyellow':'#adff2f',
+            'honeydew':'#f0fff0',
+            'hotpink':'#ff69b4',
+            'indianred':'#cd5c5c',
+            'indigo':'#4b0082',
+            'ivory':'#fffff0',
+            'khaki':'#f0e68c',
+            'lavender':'#e6e6fa',
+            'lavenderblush':'#fff0f5',
+            'lawngreen':'#7cfc00',
+            'lemonchiffon':'#fffacd',
+            'lightblue':'#add8e6',
+            'lightcoral':'#f08080',
+            'lightcyan':'#e0ffff',
+            'lightgoldenrodyellow':'#fafad2',
+            'lightgrey':'#d3d3d3',
+            'lightgreen':'#90ee90',
+            'lightpink':'#ffb6c1',
+            'lightsalmon':'#ffa07a',
+            'lightseagreen':'#20b2aa',
+            'lightskyblue':'#87cefa',
+            'lightslategray':'#778899',
+            'lightsteelblue':'#b0c4de',
+            'lightyellow':'#ffffe0',
+            'lime':'#00ff00',
+            'limegreen':'#32cd32',
+            'linen':'#faf0e6',
+            'magenta':'#ff00ff',
+            'maroon':'#800000',
+            'mediumaquamarine':'#66cdaa',
+            'mediumblue':'#0000cd',
+            'mediumorchid':'#ba55d3',
+            'mediumpurple':'#9370d8',
+            'mediumseagreen':'#3cb371',
+            'mediumslateblue':'#7b68ee',
+            'mediumspringgreen':'#00fa9a',
+            'mediumturquoise':'#48d1cc',
+            'mediumvioletred':'#c71585',
+            'midnightblue':'#191970',
+            'mintcream':'#f5fffa',
+            'mistyrose':'#ffe4e1',
+            'moccasin':'#ffe4b5',
+            'navajowhite':'#ffdead',
+            'navy':'#000080',
+            'oldlace':'#fdf5e6',
+            'olive':'#808000',
+            'olivedrab':'#6b8e23',
+            'orange':'#ffa500',
+            'orangered':'#ff4500',
+            'orchid':'#da70d6',
+            'palegoldenrod':'#eee8aa',
+            'palegreen':'#98fb98',
+            'paleturquoise':'#afeeee',
+            'palevioletred':'#d87093',
+            'papayawhip':'#ffefd5',
+            'peachpuff':'#ffdab9',
+            'peru':'#cd853f',
+            'pink':'#ffc0cb',
+            'plum':'#dda0dd',
+            'powderblue':'#b0e0e6',
+            'purple':'#800080',
+            'red':'#ff0000',
+            'rosybrown':'#bc8f8f',
+            'royalblue':'#4169e1',
+            'saddlebrown':'#8b4513',
+            'salmon':'#fa8072',
+            'sandybrown':'#f4a460',
+            'seagreen':'#2e8b57',
+            'seashell':'#fff5ee',
+            'sienna':'#a0522d',
+            'silver':'#c0c0c0',
+            'skyblue':'#87ceeb',
+            'slateblue':'#6a5acd',
+            'slategray':'#708090',
+            'snow':'#fffafa',
+            'springgreen':'#00ff7f',
+            'steelblue':'#4682b4',
+            'tan':'#d2b48c',
+            'teal':'#008080',
+            'thistle':'#d8bfd8',
+            'tomato':'#ff6347',
+            'turquoise':'#40e0d0',
+            'violet':'#ee82ee',
+            'wheat':'#f5deb3',
+            'white':'#ffffff',
+            'whitesmoke':'#f5f5f5',
+            'yellow':'#ffff00',
+            'yellowgreen':'#9acd32'
+        },
+        nameToHex = function (name) {
+            var hex = colorNames[name.toLowerCase()];
+        
+            if (hex) {
+                return fixHex(hex);
+            }
+
+            return false;
+        },
+        nameToHsv = function (name) {
+            var hex = nameToHex(name);
+
+            if (hex) {
+                return hexToHsv(hex);
+            }
+            
+            return false;
+        },
+        nameToHsva = function (name) {
+            return hsvToHsva(nameToHsv(name));
         };
 
     // Color Static methods
@@ -231,4 +446,8 @@
     Color.rgbaToRgb = rgbaToRgb;
     Color.rgbToRgba = rgbToRgba;
     Color.fixHex = fixHex;
+    Color.colorNames = colorNames;
+    Color.nameToHex = nameToHex;
+    Color.nameToHsv = nameToHsv;
+    Color.nameToHsva = nameToHsva;
 }).call(this);
--- example.js
+++ example.js
@@ -1,22 +1,62 @@
 (function () {
+
+   var body = document.getElementsByTagName('body')[0];
+
+   function outputColorTest (testName, color, expectedHex, expectedHsv, expectedRgb, expectedRgba) {
+      var html = '<div class="panel panel-default"><div class="panel-heading"><h4>' + testName + '</h4></div>';
+
+      var hex = color.getHex(),
+         hsv = color.getHsv().toString(),
+         rgb = color.getRgb().toString(),
+         rgba = color.getRgba().toString();
+
+      html += '<div class="panel-body"><table class="table"><thead><tr><th></th><th>Expected</th><th>Result</th></tr></thead>';
+      html += '<tbody><tr class="' + (hex === expectedHex ? 'success' : 'danger') + '"><td>Hex</td>';
+
+
+      // hex
+      html += ('<td>' + expectedHex + '</td>');
+      html += ('<td>' + hex + '</td></tr>');
+
+      // hsv
+      html += ('<tr class="' + (hsv === expectedHsv ? 'success' : 'danger') + '"><td>Hsv</td>');
+      html += ('<td>' + expectedHsv + '</td>');
+      html += ('<td>' + hsv + '</td></tr>');
+
+      // rgb
+      html += ('<tr class="' + (rgb === expectedRgb ? 'success' : 'danger') + '"><td>Rgb</td>');
+      html += ('<td>' + expectedRgb + '</td>');
+      html += ('<td>' + rgb + '</td></tr>');
+
+      // rgba
+      html += ('<tr class="' + (rgba === expectedRgba ? 'success' : 'danger') + '"><td>Rgba</td>');
+      html += ('<td>' + expectedRgba + '</td>');
+      html += ('<td>' + rgba + '</td></tr>');
+
+      html += '</tbody></table></div></div>';
+
+      body.innerHTML += html;
+   }
+
    window.color = new Color({ hex: "fff" });
-   console.log("white: ", color.getHex(), color.getHsv(), color.getRgb(), color.getRgba());
+   outputColorTest('window.color = new Color({ hex: "fff" })', color, 'ffffff', '0,0,1', '255,255,255', '255,255,255,1');
    color.set("ffffff");
-   console.log("white: ", color.getHex(), color.getHsv(), color.getRgb(), color.getRgba());
+   outputColorTest('color.set("ffffff")', color, 'ffffff', '0,0,1', '255,255,255', '255,255,255,1');
    color.set("rgb(255,0,0)");
-   console.log("red: ", color.getHex(), color.getHsv(), color.getRgb(), color.getRgba());
+   outputColorTest('color.set("rgb(255,0,0)")', color, 'ff0000', '0,1,1', '255,0,0', '255,0,0,1');
    color.set("rgb(255,0,0, 0.5)");
-   console.log("half red: ", color.getHex(), color.getHsv(), color.getRgb(), color.getRgba());
+   outputColorTest('color.set("rgb(255,0,0,0.5)")', color, 'ff0000', '0,1,1', '255,0,0', '255,0,0,0.5');
    color.set([0, 0, 0, 0.5]);
-   console.log("half black: ", color.getHex(), color.getHsv(), color.getRgb(), color.getRgba());
+   outputColorTest('color.set([0, 0, 0, 0.5])', color, '000000', '0,0,0', '0,0,0', '0,0,0,0.5');
    color.set([0,0,0]);
-   console.log("black: ", color.getHex(), color.getHsv(), color.getRgb(), color.getRgba());
+   outputColorTest('color.set([0, 0, 0])', color, '000000', '0,0,0', '0,0,0', '0,0,0,1');
    color.set({ hsv: color.getHsv() });
-   console.log("black: ", color.getHex(), color.getHsv(), color.getRgb(), color.getRgba());
+   outputColorTest('color.set({ hsv: color.getHsv() })', color, '000000', '0,0,0', '0,0,0', '0,0,0,1');
    color.set({ rgb: color.getRgb() });
-   console.log("black: ", color.getHex(), color.getHsv(), color.getRgb(), color.getRgba());
+   outputColorTest('color.set({ rgb: color.getRgb() })', color, '000000', '0,0,0', '0,0,0', '0,0,0,1');
    color.set({ rgba: [0,255,0,0.5] });
-   console.log("half lime: ", color.getHex(), color.getHsv(), color.getRgb(), color.getRgba());
+   outputColorTest('color.set({ rgba: [0,255,0,0.5] })', color, '00ff00', '0.3333333333333333,1,1', '0,255,0', '0,255,0,0.5');
    color = new Color();
-   console.log("parameterless instantiation: ", color.getHex(), color.getHsv(), color.getRgb(), color.getRgba());
+   outputColorTest('color = new Color()', color, '000000', '0,0,0', '0,0,0', '0,0,0,0');
+   
 })();
